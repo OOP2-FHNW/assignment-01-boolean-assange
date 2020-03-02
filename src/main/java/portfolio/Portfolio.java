73fe294849450dd578708a65fa9fa7f6c778d787
Zweiacker Cyrill (s) <cyrill.zweiacker@students.fhnw.ch>
2020-03-02 13:00:04 +0100

    - Implemented the code, so the tests succeed! :)

--- src/main/java/portfolio/Portfolio.java
+++ src/main/java/portfolio/Portfolio.java
@@ -1,5 +1,52 @@
 package portfolio;
 
-public class Portfolio {
-    //TODO Complete this implementation and make it generic
+import portfolio.investments.Investment;
+import portfolio.investments.Share;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Portfolio<T extends Investment> {
+
+    private List<Share> myShares = new ArrayList<>();
+
+    public boolean contains(Share s) {
+        return myShares.contains(s);
+    }
+
+    public void buy(Share s) {
+        if (myShares.contains(s)) {
+            for (Share sh : myShares) {
+                if (sh.equals(s)) {
+                    sh.setCount(sh.getCount() + s.getCount());
+                }
+            }
+        } else {
+            myShares.add(s);
+        }
+    }
+
+    public void sell(String s, int amount) {
+        for (Share sh : myShares) {
+            if(sh.getTitel().equals(s)){
+                if(sh.getCount() == amount){
+                    sh.setCount(sh.getCount()-amount);
+                    myShares.remove(sh);
+                    return;
+                } else {
+                    sh.setCount(sh.getCount() - amount);
+                }
+            }
+        }
+    }
+
+    public Object getShare(String s) {
+        for (Share sh : myShares) {
+            if(sh.getTitel().equals(s)) {
+                return sh;
+            }
+        }
+        return null;
+    }
+
 }
--- src/test/java/portfolio/PortfolioTest.java
+++ src/test/java/portfolio/PortfolioTest.java
@@ -1,6 +1,7 @@
 package portfolio;
 
 import org.junit.jupiter.api.Test;
+import portfolio.investments.Share;
 
 import static org.junit.jupiter.api.Assertions.*;
 
